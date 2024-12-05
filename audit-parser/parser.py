303ff314e2072bd3fd8fa9018c6dbc3f83788767
Godwin J <godwin.j@MacBook-Pro-7.local>
2024-12-05 15:06:25 +0530

    classifier logic and ignore steps

--- audit-parser/parser.py
+++ audit-parser/parser.py
@@ -16,11 +16,6 @@ class Parser:
         trigger_exports = self.data.get("trigger", {}).get("exports", {})
         trigger_context = trigger_exports.get("context", {})
 
-        # Always extract workflow related (id, trace_id, name, version) from trigger context
-        # For trigger type, threadId, shipperId, agentId, extract from email_generate_event_data if present OTHERWISE from trigger context
-
-        # For classifers we will have a step with naming pattern as {triggerType}_generate_event_data and props of this step will have shipper, agent details
-        # EntityType and Id as null
         email_event_data = (
             trigger_exports
             .get("event", {})
@@ -28,9 +23,24 @@ class Parser:
             .get("email_generate_event_data", {})
             .get("$return_value", {})
         )
+        # Always extract workflow related (id, trace_id, name, version) from trigger context
+        # For trigger type, threadId, shipperId, agentId, extract from email_generate_event_data if present OTHERWISE from trigger context
+
+        # For classifers we will have a step with naming pattern as {triggerType}_generate_event_data and props of this step will have shipper, agent details
+        # EntityType and Id as null
+        workflow_name = trigger_context.get("workflow_name", "")
+        # if workflow name contains classifier, then extract shipper, agent details from the classifier step
+        if "classifier" in workflow_name.lower():
+            for step_name, step_data in self.data.get("steps", {}).items():
+                if not isinstance(step_data, dict):
+                    continue
+                if "generate_event_data" in step_name:
+                    trigger_exports = step_data.get("exports", {})
+                    email_event_data = trigger_exports.get("$return_value", {})
+                    break
 
         return {
-            "workflow_id": email_event_data.get("workflow_id", ""),
+            "workflow_id": trigger_context.get("workflow_id", ""),
             "workflow_name": trigger_context.get("workflow_name", ""),
             "platform_version": trigger_context.get("platform_version", ""),
 
@@ -49,9 +59,11 @@ class Parser:
         first_failure = "SUCCESS"
         first_failure_reason = ""
         final_comment = ""
-
+        ignore_steps = ["store_workflow_steps_s3", "push-workflow-run-log"]
         for step_name, step_data in steps.items():
             if not isinstance(step_data, dict):
+                continue
+            if step_name in ignore_steps:
                 continue
 
             step_error = step_data.get("error", {})
@@ -73,8 +85,8 @@ class Parser:
 
                 "shipper_id": trigger_data["shipper_id"],
                 "agent_id": trigger_data["agent_id"],
-                "entity_type": "", # TODO: need to evaluate .. agent... tracy LOAD, SAM: ORDER
-                "entity_id": "", # TODO: need to evaluate
+                "entity_type": None, # TODO: need to evaluate .. agent... tracy LOAD, SAM: ORDER
+                "entity_id": None, # TODO: need to evaluate
 
                 "workflow_id": trigger_data["workflow_id"],
                 "step_id": step_data.get("id", ""),
@@ -96,7 +108,8 @@ class Parser:
         return {
             # "first_failure": first_failure,
             "first_failure_reason": first_failure_reason,
-git             "workflow_audit": workflow_audit
+            "final_comment": final_comment,
+            "workflow_audit": workflow_audit
         }
 
     def parse_workflow(self):
@@ -113,7 +126,8 @@ git             "workflow_audit": workflow_audit
         workflow_run = {
             "id": str(uuid4()),
             "exec_id": self.data.get("id"),
-            "trace_id": self.data.get("request_id"),
+            # "trace_id": self.data.get("request_id"),
+            "trace_id": trigger_data["thread_id"], 
             "parent_exec_id": "", # TODO: Get this for identifying child workflows
 
             "workflow_id": self.data.get("workflow_id"),
@@ -124,8 +138,8 @@ git             "workflow_audit": workflow_audit
             "shipper_id": trigger_data.get("shipper_id"),
             "agent": trigger_data.get("agent_id"),
 
-            "trigger_type": trigger_data.get("type"), # trigger, EMAIL, IM
-            "trigger_id": trigger_data.get("id"), # thread_id of the email or IM
+            "trigger_type": trigger_data["trigger_type"], # trigger, EMAIL, IM
+            "trigger_id": trigger_data["thread_id"], # (not request_id from data itself) thread_id of the email or IM
             "trigger": "", # TODO: trigger context... eg. holdover_document, customer_query
 
             "entity_type": "", # TODO: need to evaluate .. agent... tracy LOAD, SAM: ORDER
--- audit-parser/test_parser.py
+++ audit-parser/test_parser.py
@@ -4,16 +4,27 @@ import unittest
 from parser import Parser
 
 class ParserTestCase(unittest.TestCase):
-
+    
     def test_parse_followup_classifier(self):
-        input_file_path = os.path.join("audit-parser", "test-input", "run-logs", "followup-classifier.json")
-        output_file_path = os.path.join("audit-parser", "test-input", "expected-output", "followup-classifier_output.json")
+        self.maxDiff = None
+        input_file_path = os.path.join("test-input", "run-logs", "followup-classifier.json")
+        
+        output_file_path = os.path.join("test-input", "expected-output", "followup-classifier_output.json")
         parser = Parser(input_file_path)
-
+        parsed_output = parser.parse_workflow()
+        
         with open(output_file_path, "r") as file:
             expected_result = json.load(file)
+        
+        def normalize_data(data):
+            if isinstance(data, dict):
+                return {k: normalize_data(v) for k, v in data.items() if k not in {"id", "timestamp"}}
+            elif isinstance(data, list):
+                return [normalize_data(item) for item in data]
+            else:
+                return data
 
-        self.assertEqual(parser.parse_workflow(), expected_result)
+        self.assertEqual(normalize_data(parsed_output), normalize_data(expected_result))
 
 if __name__ == "__main__":
     unittest.main()
