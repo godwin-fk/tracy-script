ca2aa5e5c4f03b33448d5fa762362d9c8c49b546
Deependra Mishra <deependra.mishra@fourkites.com>
2024-12-05 17:14:20 +0530

    Updating gitignore

--- audit-parser/test_parser.py
+++ audit-parser/test_parser.py
@@ -4,18 +4,18 @@ import unittest
 from parser import Parser
 
 class ParserTestCase(unittest.TestCase):
-    
+
     def test_parse_followup_classifier(self):
         self.maxDiff = None
         input_file_path = os.path.join("test-input", "run-logs", "followup-classifier.json")
-        
+
         output_file_path = os.path.join("test-input", "expected-output", "followup-classifier_output.json")
         parser = Parser(input_file_path)
         parsed_output = parser.parse_workflow()
-        
+
         with open(output_file_path, "r") as file:
             expected_result = json.load(file)
-        
+
         def normalize_data(data):
             if isinstance(data, dict):
                 return {k: normalize_data(v) for k, v in data.items() if k not in {"id", "timestamp"}}
