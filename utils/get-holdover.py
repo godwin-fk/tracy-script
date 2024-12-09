3886e7e74f058b670e48c1cb4aff6ae3c41e454b
Godwin J <godwin.j@MacBook-Pro-7.local>
2024-12-09 13:37:32 +0530

    add date and fix merge_xlsx

--- utils/get-holdover.py
+++ utils/get-holdover.py
@@ -4,7 +4,7 @@ import base64
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from openpyxl import load_workbook, Workbook
 import pandas as pd
 
@@ -51,9 +51,12 @@ def search_emails_with_attachments(gmail_service, query, save_path):
     os.makedirs(save_path, exist_ok=True)
 
     try:
+        # Dict to store received date along with filePath
+        received_dates = {}
         # Search emails matching the query
         results = gmail_service.users().messages().list(userId="me", q=query).execute()
         messages = results.get("messages", [])
+        
 
         if not messages:
             print("No emails found matching the query.")
@@ -61,6 +64,10 @@ def search_emails_with_attachments(gmail_service, query, save_path):
 
         for message in messages:
             msg = gmail_service.users().messages().get(userId="me", id=message["id"]).execute()
+            received_timestamp = int(msg.get("internalDate")) / 1000
+            received_date = datetime.fromtimestamp(received_timestamp, tz=timezone.utc).strftime("%d:%m:%Y")
+
+            print(received_date)
             subject = next(
                 (header["value"] for header in msg["payload"]["headers"] if header["name"] == "Subject"), "No Subject"
             )
@@ -76,13 +83,16 @@ def search_emails_with_attachments(gmail_service, query, save_path):
                         ).execute()
                         data = attachment["data"]
                         filepath = get_unique_filepath(os.path.join(save_path, part["filename"]))
+                        received_dates[filepath] = received_date
                         with open(filepath, "wb") as f:
                             f.write(base64.urlsafe_b64decode(data))
                         print(f"Downloaded Attachment: {part['filename']}")
+        # print(received_date_filePath)
+        return received_dates
     except HttpError as error:
         print(f"An error occurred: {error}")
         
-def merge_xlsx_files(directory, output_file="./temp/merged_holdover_reports.xlsx"):
+def merge_xlsx_files(directory, received_dates, output_file="./temp/merged_holdover_reports.xlsx"):
         temp_directory = os.path.dirname(output_file)
         os.makedirs(temp_directory, exist_ok=True)
         """Merge all Excel files in a directory into a single workbook."""
@@ -91,23 +101,41 @@ def merge_xlsx_files(directory, output_file="./temp/merged_holdover_reports.xlsx
         merged_sheet = merged_workbook.active
         merged_sheet.title = "Merged Data"
         headers_written = False
+        
+        headers = [
+            "Load Number", "CARRIER", "CONTAINER ID", "DESTINATION CITY", 
+            "DESTINATION STATE", "DD DATE", "DD TIME", "BILL DATE", 
+            "BILL TIME", "ZDLT LATE CODE", "ON TIME? (Y/N)", "SPLIT? (Y/N)", 
+            "FRESH PRIORITY STO? (Y/N)", "NOTES/COMMENTS"
+        ]
 
         merged_data = pd.DataFrame()
-        for file_name in os.listdir(directory):
+        for i,file_name in enumerate(os.listdir(directory)):
             if file_name.endswith(".xlsx"):
                 file_path = os.path.join(directory, file_name)
+    
+                df = pd.read_excel(file_path, sheet_name=0,header=None)
+                plant_row_index = df[df.eq("PLANT:").any(axis=1)].index[0]
+                plant_column_index = df.loc[plant_row_index].eq("PLANT:").idxmax()
+                result_column_index = df.columns.get_loc(plant_column_index) + 1
+                FACILITY = df.iloc[plant_row_index, result_column_index]
                 
-                df = pd.read_excel(file_path, sheet_name=0)
-                shipment_row_index = df[df.eq("PLANT:").any(axis=1)].index[0]
-                column_index = df.loc[shipment_row_index].eq("PLANT:").idxmax()
-                result_column_index = df.columns.get_loc(column_index) + 1
-                FACILITY = df.iloc[shipment_row_index, result_column_index]
+                header_row_index = df[df.eq("SHIPMENT NUMBER").any(axis=1)].index[0]
+                # print(header_row_index)
+                data = df.iloc[header_row_index+1:].reset_index(drop=True)
+                data.columns = headers
                 
-                header_row_index = shipment_row_index + 2  
-                data = df.iloc[header_row_index:].reset_index(drop=True)
-                data.columns = df.iloc[header_row_index - 1]  # Set the proper headers
+                # data.columns = df.iloc[header_row_index]  # Set the proper headers
+                
+                # print(data)
+                # data.columns = [str(col).strip() for col in data.columns]
 
-                data['PLANT'] = FACILITY     
+                data['PLANT'] = FACILITY 
+                if file_path in received_dates:
+                    data['RECEIVED DATE'] = received_dates[file_path]    
+                    
+                # data.rename( columns={'Unnamed: 14':'PLANT'}, inplace=True )
+                # data.rename( columns={'Unnamed: 15':'RECEIVED DATE'}, inplace=True )
 
                 merged_data = pd.concat([merged_data, data], ignore_index=True)
         merged_data.to_excel(output_file, index=False)
@@ -225,7 +253,7 @@ if __name__ == "__main__":
     save_path = "attachments"
     output_file = "../temp/output_data.xlsx"
 
-    start_date = "2024-12-01"
+    start_date = "2024-12-05"
     end_date = "2024-12-05"
 
     start_timestamp = int(datetime.strptime(start_date, "%Y-%m-%d").timestamp())
@@ -236,10 +264,10 @@ if __name__ == "__main__":
 
     search_query = f'holdover OR "hold over" has:attachment in:anywhere after:{start_timestamp} before:{end_timestamp}'
 
-    # gmail_service = get_gmail_service(shipper_id, agent_id)
-    # search_emails_with_attachments(gmail_service, search_query, save_path)
+    gmail_service = get_gmail_service(shipper_id, agent_id)
+    received_dates = search_emails_with_attachments(gmail_service, search_query, save_path)
 
-    input_file = merge_xlsx_files(save_path)
+    input_file = merge_xlsx_files(save_path,received_dates)
     clean_excel(input_file, output_file)
     update_headers(output_file)
     
